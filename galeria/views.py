687370442fbfbc9bcceb37608976fd32a9379e29
Daniel-Alves <contato@danielalves.tec.br>
2026-02-12 09:28:43 -0300

    1 card

--- galeria/views.py
+++ galeria/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
+
+
 def index(request):
-    return render(request, 'galeria/index.html')
+    dados = {
+    1: {"nome" : "Nebulosa de Carina", "legenda" : "webbtelescope.org / NASA/ James Webb"},
+    2: {"nome" : "Galaxia NGX 1079", "legenda" : "nasa.org / NASA / Hubble"}
+    }
+    
+    return render(request, 'galeria/index.html', {"cards": dados})
 # Create your views here.
 def imagem(request):
     return render(request, 'galeria/imagem.html')
